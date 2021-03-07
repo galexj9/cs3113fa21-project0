883bc6062ee0ccc59b4f7324c555c6bac59b4758
galexj9 <43096683+galexj9@users.noreply.github.com>
2021-03-07 17:45:09 -0600

    fixing pointer logic

--- source.c
+++ source.c
@@ -7,25 +7,25 @@
 //write a list to stdout with each char in descending order of count
 int main() {
   //my list of all unicode characters (stored as lists) and their count
-  Node* list = malloc(sizeof(*Node));
+  Node* list = (Node *) malloc(sizeof(Node));
 
   int c; //getChar() returns an unsigned int (8 bits) cast to int
   //loop through stdin to construct the unicode count list
-  while((c = getChar()) != EOF) {
+  while((c = getchar()) != EOF) {
     //create a new list to store a single unicode character
-    List* character = malloc(sizeof(*Node));
+    Node* character = (Node *) malloc(sizeof(Node));
     //if the first bit is 1, there are more characters to follow
     while(c & 0x80 == 1) {
       push(character, c);
-      c = getChar();
+      c = getchar();
     }
     //if the first bit is 0 then c is the final or only char in the unicode
     push(character, c);
     //add the completed character to the unicode list
-      if (get(list, character) == null)
-        push(list, character);
+      if (get(list, &character) == null)
+        push(list, &character);
       //increment the count
-      get(list, character)->count += 1;
+      get(list, &character)->count += 1;
   }
 
   //sort the list in descending order of occurrence
